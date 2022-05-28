e3db0b8f597ed6901260033906984b33ed458733
odhako <odhako@gmail.com>
2022-05-28 10:47:35 +0400

    06

--- gendiff/__init__.py
+++ gendiff/__init__.py
@@ -1,3 +1,3 @@
-from .program.gendiff import generate_diff, stylish
+from .program.gendiff import generate_diff
 from .program.cli import cli
-__all__ = ['generate_diff', 'cli', 'stylish']
+__all__ = ['generate_diff', 'cli']
--- gendiff/program/cli.py
+++ gendiff/program/cli.py
@@ -1,17 +1,16 @@
 import argparse
-from gendiff import generate_diff  # stylish
+from gendiff import generate_diff
 
 
 def cli():
     parser = argparse.ArgumentParser(
         description='Compares two configuration files and shows a difference.'
     )
-    parser.add_argument('-f', '--format', help='set format of output', choices=['stylish', ])
+    parser.add_argument('-f', '--format', help='set format of output')  # choices=['stylish', ]
     parser.add_argument('first_file')
     parser.add_argument('second_file')
     args = parser.parse_args()
-    if args.format == 'stylish':
-        pass
-        # print(generate_diff(args.first_file, args.second_file, stylish))
-    else:
-        print(generate_diff(args.first_file, args.second_file))
+    # if args.format == 'stylish':
+    #     print(generate_diff(args.first_file, args.second_file, stylish))
+    # else:
+    print(generate_diff(args.first_file, args.second_file))
