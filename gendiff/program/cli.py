5c82407d967b0471d354e21881adac649246bdbb
odhako <odhako@gmail.com>
2022-06-02 08:30:18 +0400

    Edit cli.py - 1 call

--- gendiff/program/cli.py
+++ gendiff/program/cli.py
@@ -7,15 +7,9 @@ def cli():
         description='Compares two configuration files and shows a difference.'
     )
     parser.add_argument('-f', '--format', help='set format of output',
-                        choices=['stylish', 'plain', 'json'])
+                        choices=['stylish', 'plain', 'json'],
+                        default='stylish')
     parser.add_argument('first_file')
     parser.add_argument('second_file')
     args = parser.parse_args()
-    if args.format == 'plain':
-        print(generate_diff(args.first_file, args.second_file, 'plain'))
-    elif args.format == 'json':
-        print(generate_diff(args.first_file, args.second_file, 'json'))
-    elif args.format == 'stylish':
-        print(generate_diff(args.first_file, args.second_file, 'stylish'))
-    else:
-        print(generate_diff(args.first_file, args.second_file))
+    print(generate_diff(args.first_file, args.second_file, args.format))
