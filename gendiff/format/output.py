796ef31644174470d66e12396341823196ab0bc8
odhako <odhako@gmail.com>
2022-06-11 14:35:27 +0400

    Add exception

--- gendiff/format/output.py
+++ gendiff/format/output.py
@@ -7,10 +7,12 @@ PLAIN = 'plain'
 JSON = 'json'
 
 
-def form_output(result, style):
+def form_output(string_input, style):
     if style == STYLISH:
-        return stylish(result)
+        return stylish(string_input)
     elif style == PLAIN:
-        return plain(result)
+        return plain(string_input)
     elif style == JSON:
-        return json_format(result)
+        return json_format(string_input)
+    else:
+        raise Exception('Unknown style')
